b6eafc09f4f6b446cdbd546ac93dcfee167e5f65
Hyunsoo <imapples@naver.com>
2025-03-21 15:20:43 +0900

    study: 가져오기와 내보내기 패턴

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-// ✅ 모듈(Module)이란 특정 데이터들의 집합(파일)
-// 모듈 가져오기(Import), 내보내기(Export)
+// // ✅ 모듈(Module)이란 특정 데이터들의 집합(파일)
+// // 모듈 가져오기(Import), 내보내기(Export)
 
-import { hello } from "./module";
+// import { hello } from "./module";
 
-console.log(hello);
+// console.log(hello);
+
+// ✅
+// import number, { str as xyz, arr, hello as h } from "./module";
+
+// console.log(number); // 123
+// console.log(xyz); // ABC
+// console.log(arr); // []
+// console.log(h); // f hello() {}
+
+import * as abc from "./module";
+
+console.log(abc);
