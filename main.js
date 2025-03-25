bb0c64facb63de104979de7574a01efff6ed2833
Hyunsoo <imapples@naver.com>
2025-03-25 21:24:48 +0900

    study: resolve, reject 그리고 에러 핸들링

--- main.js
+++ main.js
@@ -192,43 +192,145 @@
 //     console.log("아바타");
 //   });
 
-// ✅ Async / Await
+// // ✅ Async / Await
 
-const a = () => {
-  return new Promise((resolve) => {
+// const a = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(1);
+//       resolve();
+//     }, 1000);
+//   });
+// };
+// const b = () => console.log(2);
+
+// const wrap = async () => {
+//   await a(); // 뒤쪽에 있는 비동기 함수의 실행을 기다린다
+//   b();
+// };
+
+// wrap();
+
+// const getMovies = (movieName) => {
+//   return new Promise((resolve) => {
+//     fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         console.log(res);
+//         resolve();
+//       });
+//   });
+// };
+
+// const wrapMovies = async () => {
+//   await getMovies("frozen"); // await는 프로미스가 반환되는 함수 앞에만!
+//   console.log("겨울왕국");
+//   await getMovies("avengers");
+//   console.log("어벤저스");
+//   await getMovies("avatar");
+//   console.log("아바타");
+// };
+
+// wrapMovies();
+
+// ✅ Resolve, Reject, 그리고 에러 핸들링
+const func1 = () => {
+  const delayAdd = (index, cb, errorCb) => {
     setTimeout(() => {
-      console.log(1);
-      resolve();
+      if (index > 10) {
+        errorCb(`${index}는 10보다 클 수 없습니다`);
+        return;
+      }
+      console.log(index);
+      cb(index + 1);
     }, 1000);
-  });
-};
-const b = () => console.log(2);
+  };
 
-const wrap = async () => {
-  await a(); // 뒤쪽에 있는 비동기 함수의 실행을 기다린다
-  b();
-};
-
-wrap();
-
-const getMovies = (movieName) => {
-  return new Promise((resolve) => {
-    fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        resolve();
-      });
-  });
+  delayAdd(
+    13,
+    (res) => console.log(res),
+    (err) => console.error(err)
+  );
 };
 
-const wrapMovies = async () => {
-  await getMovies("frozen"); // await는 프로미스가 반환되는 함수 앞에만!
-  console.log("겨울왕국");
-  await getMovies("avengers");
-  console.log("어벤저스");
-  await getMovies("avatar");
-  console.log("아바타");
+// func1();/
+
+const func2 = () => {
+  const delayAdd = (index) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (index > 10) {
+          reject(`${index}는 10보다 클 수 없습니다`);
+          return; // resolve 부분만 실행 안되는거고 나머지는 실행될 수 있어서 return 필요
+        }
+        console.log(index);
+        resolve(index + 1);
+      }, 1000);
+    });
+  };
+  delayAdd(13)
+    .then((res) => console.log(res)) // resolve로 들어감
+    .catch((error) => console.log(error))
+    .finally(console.log("Done!")); // reject로 들어감
+
+  const wrap = async () => {
+    const res = await delayAdd(13);
+    console.log(res);
+  };
+
+  const wrap1 = async () => {
+    try {
+      const res = await delayAdd(12); // try 문 안에서는 에러 발생하면 그 이후 코드는 실행하지 않음
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // 항상 실행
+      console.log("Done!");
+    }
+  };
+
+  // wrap();
+  wrap1();
+};
+// func2();
+
+const func3 = () => {
+  const getMovies = (movieName) => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+        .then((res) => res.json())
+        .then((json) => {
+          if (json.Response === "False") {
+            reject(json.Error);
+          }
+          resolve(json);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  let loading = true;
+
+  getMovies("avengers")
+    .then((movies) => console.log("영화 목록: ", movies))
+    .catch((error) => console.log("에러 발생:", error))
+    .finally(() => (loading = false));
+
+  const wrap = async () => {
+    try {
+      const movies = await getMovies("avengers");
+      console.log("영화 목록: ", movies);
+    } catch (error) {
+      console.log("에러 발생: ", error);
+    } finally {
+      loading = false;
+    }
+  };
+
+  wrap();
 };
 
-wrapMovies();
+func3();
