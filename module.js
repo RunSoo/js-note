fb2c5810588071263672722645f62df98c01f17c
Hyunsoo <imapples@naver.com>
2025-03-25 15:16:20 +0900

    init: 비동기 스터디 시작

--- module.js
+++
@@ -1,9 +0,0 @@
-// export const hello = "Hello world";
-
-// 1️⃣ 기본 내보내기 -> 하나의 데이터만 내보낼 수 있음
-export default 123;
-
-// 2️⃣ 이름 내보내기 -> 여러 개 가능
-export const str = "ABC";
-export const arr = [];
-export function hello() {}
