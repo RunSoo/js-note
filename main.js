29147c0aad22a4a62b718297acfefa152c745766
Hyunsoo <imapples@naver.com>
2025-03-25 16:14:23 +0900

    study: async await 패턴

--- main.js
+++ main.js
@@ -84,89 +84,132 @@
 
 // callbackHell();
 
-// ✅ Promise
+// // ✅ Promise
 
-const func1 = () => {
-  const a = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log(1);
-        resolve();
-      }, 1000);
-    });
-  };
+// const func1 = () => {
+//   const a = () => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         console.log(1);
+//         resolve();
+//       }, 1000);
+//     });
+//   };
 
-  const b = () => console.log(2);
+//   const b = () => console.log(2);
 
-  a().then(() => {
-    b();
+//   a().then(() => {
+//     b();
+//   });
+// };
+
+// // func1();
+
+// const func2 = () => {
+//   const a = () => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         console.log(1);
+//         resolve();
+//       }, 1000);
+//     });
+//   };
+//   const b = () => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         console.log(2);
+//         resolve();
+//       }, 1000);
+//     });
+//   };
+//   const c = () => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         console.log(3);
+//         resolve();
+//       }, 1000);
+//     });
+//   };
+//   const d = () => console.log(4);
+
+//   // // 🚨 여전히 콜백 지옥과 유사
+//   // a().then(() => {
+//   //   b().then(() => {
+//   //     c().then(() => {
+//   //       d();
+//   //     });
+//   //   });
+//   // });
+
+//   // a()
+//   //   .then(() => {
+//   //     return b();
+//   //   })
+//   //   .then(() => {
+//   //     return c();
+//   //   })
+//   //   .then(() => {
+//   //     d();
+//   //   });
+
+//   // a()
+//   //   .then(() => b())
+//   //   .then(() => c())
+//   //   .then(() => {
+//   //     d();
+//   //   });
+
+//   a()
+//     .then(b)
+//     .then(c)
+//     .then(d)
+//     .then(() => console.log("done"));
+// };
+
+// // func2();
+
+// const getMovies = (movieName) => {
+//   return new Promise((resolve) => {
+//     fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         console.log(res);
+//         resolve();
+//       });
+//   });
+// };
+
+// getMovies("frozen")
+//   .then(() => {
+//     console.log("겨울왕국");
+//     return getMovies("avengers");
+//   })
+//   .then(() => {
+//     console.log("어벤저스");
+//     return getMovies("avatar");
+//   })
+//   .then(() => {
+//     console.log("아바타");
+//   });
+
+// ✅ Async / Await
+
+const a = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(1);
+      resolve();
+    }, 1000);
   });
 };
+const b = () => console.log(2);
 
-// func1();
-
-const func2 = () => {
-  const a = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log(1);
-        resolve();
-      }, 1000);
-    });
-  };
-  const b = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log(2);
-        resolve();
-      }, 1000);
-    });
-  };
-  const c = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log(3);
-        resolve();
-      }, 1000);
-    });
-  };
-  const d = () => console.log(4);
-
-  // // 🚨 여전히 콜백 지옥과 유사
-  // a().then(() => {
-  //   b().then(() => {
-  //     c().then(() => {
-  //       d();
-  //     });
-  //   });
-  // });
-
-  // a()
-  //   .then(() => {
-  //     return b();
-  //   })
-  //   .then(() => {
-  //     return c();
-  //   })
-  //   .then(() => {
-  //     d();
-  //   });
-
-  // a()
-  //   .then(() => b())
-  //   .then(() => c())
-  //   .then(() => {
-  //     d();
-  //   });
-
-  a()
-    .then(b)
-    .then(c)
-    .then(d)
-    .then(() => console.log("done"));
+const wrap = async () => {
+  await a(); // 뒤쪽에 있는 비동기 함수의 실행을 기다린다
+  b();
 };
 
-// func2();
+wrap();
 
 const getMovies = (movieName) => {
   return new Promise((resolve) => {
@@ -179,15 +222,13 @@ const getMovies = (movieName) => {
   });
 };
 
-getMovies("frozen")
-  .then(() => {
-    console.log("겨울왕국");
-    return getMovies("avengers");
-  })
-  .then(() => {
-    console.log("어벤저스");
-    return getMovies("avatar");
-  })
-  .then(() => {
-    console.log("아바타");
-  });
+const wrapMovies = async () => {
+  await getMovies("frozen"); // await는 프로미스가 반환되는 함수 앞에만!
+  console.log("겨울왕국");
+  await getMovies("avengers");
+  console.log("어벤저스");
+  await getMovies("avatar");
+  console.log("아바타");
+};
+
+wrapMovies();
